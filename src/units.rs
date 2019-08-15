4417f85c3ac147d5cbe408b1cd593ad4d8b8603a
Fraser Balch <farseer.ulwithy@gmail.com>
2019-08-14 22:31:06 -0600

    created macro_rules for unit declarations

--- src/units.rs
+++ src/units.rs
@@ -2,115 +2,40 @@ pub trait Unit : 'static + Send + Sync + Copy + PartialEq + Default {
     fn symbol() -> Option<&'static str>;
 }
 
+macro_rules! define_unit {
+    ($unit:ident) => (
+        #[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
+        pub struct $unit;
 
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Unitless;
+        impl Unit for $unit {
+            fn symbol() -> Option<&'static str> { None }
+        }
+    );
+    ($unit:ident, $symbol:literal) => (
+        #[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
+        pub struct $unit;
 
-impl Unit for Unitless {
-    fn symbol() -> Option<&'static str> { None }
+        impl Unit for $unit {
+            fn symbol() -> Option<&'static str> { Some($symbol) }
+        }
+    )
 }
 
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Seconds;
-
-impl Unit for Seconds {
-    fn symbol() -> Option<&'static str> { Some("s") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct SecondsSquared;
-
-impl Unit for SecondsSquared {
-    fn symbol() -> Option<&'static str> { Some("s²") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Kilograms;
-
-impl Unit for Kilograms {
-    fn symbol() -> Option<&'static str> { Some("kg") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Meters;
-
-impl Unit for Meters {
-    fn symbol() -> Option<&'static str> { Some("m") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct MetersPerSecond;
-
-impl Unit for MetersPerSecond {
-    fn symbol() -> Option<&'static str> { Some("m/s") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct MetersPerSecondSquared;
-
-impl Unit for MetersPerSecondSquared {
-    fn symbol() -> Option<&'static str> { Some("m/s²") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Kelvin;
-
-impl Unit for Kelvin {
-    fn symbol() -> Option<&'static str> { Some("K") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Newtons;
-
-impl Unit for Newtons {
-    fn symbol() -> Option<&'static str> { Some("N") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct Joules;
-
-impl Unit for Joules {
-    fn symbol() -> Option<&'static str> { Some("J") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct JoulesPerKilogram;
-
-impl Unit for JoulesPerKilogram {
-    fn symbol() -> Option<&'static str> { Some("J/kg") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct JoulesPerSecond;
-
-impl Unit for JoulesPerSecond {
-    fn symbol() -> Option<&'static str> { Some("J/s") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct KilogramsPerSecond;
-
-impl Unit for KilogramsPerSecond {
-    fn symbol() -> Option<&'static str> { Some("kg/s") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct KilogramsPerMeterCubed;
-
-impl Unit for KilogramsPerMeterCubed {
-    fn symbol() -> Option<&'static str> { Some("kg/m³") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct MetersSquared;
-
-impl Unit for MetersSquared {
-    fn symbol() -> Option<&'static str> { Some("m²") }
-}
-
-#[derive(Debug, Default, PartialEq, Copy, Clone, PartialOrd)]
-pub struct MetersCubed;
-
-impl Unit for MetersCubed {
-    fn symbol() -> Option<&'static str> { Some("m³") }
-}
+define_unit!(Unitless);
+define_unit!(Seconds, "s");
+define_unit!(SecondsSquared, "s²");
+define_unit!(Kilograms, "kg");
+define_unit!(Meters, "m");
+define_unit!(MetersSquared, "m²");
+define_unit!(MetersCubed, "m³");
+define_unit!(MetersPerSecond, "m/s");
+define_unit!(MetersPerSecondSquared, "m/s²");
+define_unit!(Kelvin, "K");
+define_unit!(Newtons, "N");
+define_unit!(Joules, "J");
+define_unit!(JoulesPerKilogram, "J/kg");
+define_unit!(JoulesPerSecond, "J/s");
+define_unit!(KilogramsPerSecond, "kg/s");
+define_unit!(KilogramsPerMeterCubed, "kg/m³");
+define_unit!(Pixels, "px");
+define_unit!(MetersPerPixel, "m/px");
